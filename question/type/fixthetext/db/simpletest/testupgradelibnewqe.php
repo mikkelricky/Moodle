2863b695681cd95cabbdc740c89b0e80944884aa
Mikkel Ricky <mikkel@mikkelricky.dk>
2012-03-11 19:06:16 +0100

    Cleaning up

---
+++ question/type/fixthetext/db/simpletest/testupgradelibnewqe.php
@@ -0,0 +1,13 @@
+<?php defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/question/engine/upgrade/simpletest/helper.php');
+
+
+/**
+ * Testing the upgrade of fixthetext question attempts.
+ *
+ * @copyright  2012 Mikkel Ricky
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_fixthetext_attempt_upgrader_test extends question_attempt_upgrader_test_base {
+}
